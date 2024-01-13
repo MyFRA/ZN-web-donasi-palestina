f25f27a984a6410fafc1c78e30ad0dae2a6651ed
MyFRA <tomyntapss@gmail.com>
2024-01-13 17:09:14 +0700

    done: Home Index Page

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import {
 } from "react-router-dom";
 import './index.css'
 import HomeIndex from './pages/home/HomeIndex';
+import 'remixicon/fonts/remixicon.css'
 
 const router = createBrowserRouter([
     {
